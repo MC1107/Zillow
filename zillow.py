7bb845eb031aa469463fbba9fd2d8b2a7fde4ffb
Melody Chen <ychen03@wesleyan.edu>
2017-06-13 13:51:16 +0800

    data inport

--- zillow.py
+++ zillow.py
@@ -1,9 +1,16 @@
 #import packages
 import numpy as np 
-import pandas as pd 
+#import pandas as pd #data structures
 import matplotlib.pyplot as plt
+import seaborn as sns #visulizations
 
 
-#read un data
+
+#read in data
 train = pd.read_csv('../input/train_2016.csv', parse_dates=["transactiondate"])
 properties = pd.read_csv('../input/properties_2016.csv')
+
+#explore train's shape
+train.shape
+train.head
+
